c88062fbe8c1052646d8f4810f8bd0a4aa15573a
Aferokript <alberto.cardinale@bk.ru>
2026-02-19 16:54:46 +0300

    Поправил названия переменных

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,14 +6,14 @@ from .models import get_duration, format_duration
 
 
 def storage_information_view(request):
-    non_closed_visits = [{}]
+    non_closed_visits = {}
     visits = Visit.objects.filter(leaved_at=None)
     for visit in visits:
         minutes = get_duration(visit)
-        duration_str = format_duration(minutes)
+        duration = format_duration(minutes)
         name = visit.passcard.owner_name
         entered_at = visit.entered_at
-        non_closed_visits.append({'who_entered': name, 'entered_at': entered_at, 'duration': duration_str})
+        non_closed_visits.append({'who_entered': name, 'entered_at': entered_at, 'duration': duration})
 
     context = {
         'non_closed_visits': non_closed_visits,
