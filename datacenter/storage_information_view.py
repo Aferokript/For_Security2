e78ccbf5b7e7cf5abe1e2cbbe4b3bc9f0ceaede7
Aferokript <alberto.cardinale@bk.ru>
2026-02-20 13:40:10 +0300

    Исправил переменные и словарь

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,18 +2,22 @@ from datacenter.models import Passcard, Visit
 from django.shortcuts import render
 import datetime
 from django.utils import timezone
-from .models import get_duration, format_duration
+from .models import get_duration, formated_duration
 
 
 def storage_information_view(request):
     non_closed_visits = {}
     visits = Visit.objects.filter(leaved_at=None)
+
     for visit in visits:
         minutes = get_duration(visit)
-        duration = format_duration(minutes)
+        fix_time = formated_duration(minutes)
         name = visit.passcard.owner_name
         entered_at = visit.entered_at
-        non_closed_visits.append({'who_entered': name, 'entered_at': entered_at, 'duration': duration})
+        non_closed_visits[who_entered] = name
+        non_closed_visits[entered_at] = entered_at
+        non_closed_visits[duration] = duration
+
 
     context = {
         'non_closed_visits': non_closed_visits,
